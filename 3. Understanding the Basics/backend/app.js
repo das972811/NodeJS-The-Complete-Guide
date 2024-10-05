20e333887ec61d2e5998dce6f88cb7979d3790de
das972811 <salasdiego54@gmail.com>
2024-10-05 13:10:04 -0500

    Basic routing request

--- 3. Understanding the Basics/backend/app.js
+++ 3. Understanding the Basics/backend/app.js
@@ -1,8 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+
+    res.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        console.log('This is a test');
+        return;
+    } else {
+        console.log('This is also a fucking test');
+        return;
+    }
+
+    res.end();
+
     // process.exit();
+
 });
 
 server.listen(3000);
