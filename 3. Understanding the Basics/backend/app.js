e6263f8e5b1895a876bec13f9e61804c73cd3821
das972811 <salasdiego54@gmail.com>
2024-10-05 21:32:00 -0500

    Completed understanding the basics

--- 3. Understanding the Basics/backend/app.js
+++ 3. Understanding the Basics/backend/app.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     const url = req.url;
+    const method = req.method;
+    console.log(method);
 
     res.setHeader('Content-Type', 'text/html');
     if (url === '/') {
