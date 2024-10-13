15b658294ecb2af71ceb67cdf67a2fc65ba76403
das972811 <salasdiego54@gmail.com>
2024-10-13 15:48:24 -0500

    Serve admin html file

--- 5. Working with Express.js/express-starter-01/routes/admin.js
+++ 5. Working with Express.js/express-starter-01/routes/admin.js
@@ -1,10 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    console.log('In the second middleware');
-    res.send('<form action="/add-product" method="POST"><input type="text" name="title" /><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
