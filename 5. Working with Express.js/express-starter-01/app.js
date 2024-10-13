29e069ebaf2d09bc682a0ebf371b5cdb00653ba5
das972811 <salasdiego54@gmail.com>
2024-10-13 15:28:46 -0500

    Added the express router

--- 5. Working with Express.js/express-starter-01/app.js
+++ 5. Working with Express.js/express-starter-01/app.js
@@ -3,27 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
     console.log('This always run');
     next();
-});
-
-app.use('/add-product', (req, res, next) => {
-    console.log('In the second middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    console.log('This is a POST REQUEST');
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In the second middleware');
-    res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
