849f564e093e185204ddc74a34eb0b8ab50be79b
das972811 <salasdiego54@gmail.com>
2024-10-13 15:57:20 -0500

    Cleaned rootdir

--- 5. Working with Express.js/express-starter-01/app.js
+++ 5. Working with Express.js/express-starter-01/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -17,7 +18,7 @@ app.use('/', (req, res, next) => {
 });
 
 app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
