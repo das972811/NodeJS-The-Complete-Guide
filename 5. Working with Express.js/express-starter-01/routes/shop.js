cff4ed605f378d5f88dd07df99194de388de24f6
das972811 <salasdiego54@gmail.com>
2024-10-13 15:46:56 -0500

    Serving html files

--- 5. Working with Express.js/express-starter-01/routes/shop.js
+++ 5. Working with Express.js/express-starter-01/routes/shop.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('In the second middleware');
-    res.send('<h1>Hello from Express!</h1>');
+    // console.log('In the second middleware');
+    // res.send('<h1>Hello from Express!</h1>');
+
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+
 });
 
 module.exports = router;
