ecaa2624d764a4220da52925d0613f9a29fc3411
das972811 <salasdiego54@gmail.com>
2024-10-13 15:19:59 -0500

    Added a basic form submission

--- 5. Working with Express.js/express-starter-01/app.js
+++ 5. Working with Express.js/express-starter-01/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use((req, res, next) => { 
-//     console.log('In the middleware');
-//     next();
-// });
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res, next) => {
+    console.log('This always run');
+    next();
+});
 
 app.use('/add-product', (req, res, next) => {
     console.log('In the second middleware');
-    res.send('<h1>The add product page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    console.log('This is a POST REQUEST');
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
